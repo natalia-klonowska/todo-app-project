2ff6e1c923668bac827646e33d9ad69c8c8e933e
Natalia Klonowska <natalia742@gmail.com>
2023-06-19 00:30:16 +0200

    add project files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,108 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { UserWarning } from './UserWarning';
+import { Todo } from './types/Todo';
+import { getTodos } from './api/todos';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+import { TodoList } from './components/TodoList';
+import { Notification } from './components/Notification';
+import { Filter } from './types/Filter';
 
-function App() {
+const USER_ID = 10606;
+
+export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [filter, setFilter] = useState<Filter>(Filter.All);
+  const [error, setError] = useState('');
+  const [tempTodo, setTempTodo] = useState<Todo | null>(null);
+  const [isUpdating, setIsUpdating] = useState(false);
+  const [updatingIds, setUpdatingIds] = useState<number[]>([]);
+
+  const hasTodos = todos.length > 0;
+
+  useEffect(() => {
+    getTodos(USER_ID)
+      .then(setTodos)
+      .catch(() => setError('Unable to load todos'));
+  }, []);
+
+  if (!USER_ID) {
+    return <UserWarning />;
+  }
+
+  const Cleaner = () => {
+    setTempTodo(null);
+    setIsUpdating(false);
+  };
+
+  const handleFilter = (newFilter: Filter) => {
+    setFilter(newFilter);
+  };
+
+  const handleError = (newError: string) => {
+    setError(newError);
+  };
+
+  const handleTempTodo = (newTodo: Todo | null) => {
+    setTempTodo(newTodo);
+  };
+
+  const handleIsUpdating = (status: boolean) => {
+    setIsUpdating(status);
+  };
+
+  const handleUpdatingIds = (ids: number[]) => {
+    setUpdatingIds(ids);
+  };
+
+  const handleLoadTodos = () => {
+    getTodos(USER_ID)
+      .then(setTodos)
+      .then(Cleaner)
+      .catch(() => setError('Unable to load todos'));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
+
+      <div className="todoapp__content">
+        <Header
+          todos={todos}
+          userId={USER_ID}
+          handleTempTodo={handleTempTodo}
+          handleError={handleError}
+          handleIsUpdating={handleIsUpdating}
+          handleUpdatingIds={handleUpdatingIds}
+        />
+
+        <TodoList
+          todos={todos}
+          filter={filter}
+          tempTodo={tempTodo}
+          handleError={handleError}
+          isUpdating={isUpdating}
+          handleIsUpdating={handleIsUpdating}
+          updatingIds={updatingIds}
+          handleUpdatingIds={handleUpdatingIds}
+          handleLoadTodos={handleLoadTodos}
+        />
+
+        {hasTodos && (
+          <Footer
+            todos={todos}
+            filter={filter}
+            handleFilter={handleFilter}
+            handleError={handleError}
+            handleIsUpdating={handleIsUpdating}
+            handleUpdatingIds={handleUpdatingIds}
+          />
+        )}
+      </div>
+
+      {error && (
+        <Notification error={error} />
+      )}
     </div>
   );
-}
-
-export default App;
+};
